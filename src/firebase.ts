196b166a5affa81fce5bc1c7f7c4c72f92e79360
moodiseo <moodiseo@gmail.com>
2025-01-15 16:38:20 +0900

    #2.3

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVj01iRUm9mujPcQjJ1443QcXSHaJzUAA",
+  authDomain: "nwitter-reloaded-69553.firebaseapp.com",
+  projectId: "nwitter-reloaded-69553",
+  storageBucket: "nwitter-reloaded-69553.firebasestorage.app",
+  messagingSenderId: "28034761370",
+  appId: "1:28034761370:web:f044cf94e2ef13593b9523",
+};
+
+const app = initializeApp(firebaseConfig);
